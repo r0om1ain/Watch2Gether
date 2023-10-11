2beaab7c7899edf0f82cf98f7dc945754c085a6e
davidsun13 <137908764+davidsun13@users.noreply.github.com>
2023-10-11 12:36:28 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Film from './components/Film';
 import DetailsFilms from './components/DetailsFilms';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path='/' element={<Film />}/>
         <Route path="/details/:filmId" element={<DetailsFilms/>} />

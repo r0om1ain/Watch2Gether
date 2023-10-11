fc6951726c8e1f8d46c17e3e4c2bbfe05bd96bb9
davidsun13 <137908764+davidsun13@users.noreply.github.com>
2023-10-11 09:44:16 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Film from './components/Film';
+import DetailsFilms from './components/DetailsFilms';
+
 
 function App() {
   return (
-    
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Film />}/>
+        <Route path="/details/:filmId" element={<DetailsFilms/>} />
+    </Routes>
+    </div>
   );
 }
 
